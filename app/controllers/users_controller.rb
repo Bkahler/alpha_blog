85367e219f5049dcb0a7cf7cffc32c6e36b60fc1
Bryan Kahler <bkahler3@gmail.com>
2016-08-07 04:10:35 +0000

    added pagination

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
   end 
   
   def index
-    @users = User.all
+    @users = User.paginate(page: params[:page], per_page: 2)
   end
   
   def create
@@ -22,6 +22,7 @@ class UsersController < ApplicationController
   end
   
   def show
+    @user_articles = @user.articles.paginate(page: params[:page], per_page: 2)
   end 
   
   def edit
