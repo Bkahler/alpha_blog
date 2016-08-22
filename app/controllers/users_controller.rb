2d6691dcb2a7fde08e6f3c1ca925acd5cb9ec16b
Bryan Kahler <bkahler3@gmail.com>
2016-08-22 02:19:40 +0000

    adding sign in upon sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,9 +14,10 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       logger.debug "#{ "-" * 10} session: #{session.id}, class: #{self}, method: #{__method__}, message: created new user #{ "-" * 10}"
       flash[:success] = "Welcome to Alpha Blog #{@user.username}"
-      redirect_to articles_path
+      redirect_to user_path(@user)
     else
       logger.warn "#{ "-" * 10} session: #{session.id}, class: #{self}, method: #{__method__}, message: User failed to create... #{ @user.errors.full_messages } #{ "-" * 10}"
       render 'new'
