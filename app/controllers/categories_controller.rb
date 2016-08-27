e774847a2a703a78917b324a5c4296d49c723fdc
Bryan Kahler <bkahler3@gmail.com>
2016-08-27 22:14:44 +0000

    fucking tests!

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -34,8 +34,6 @@ class CategoriesController < ApplicationController
     if !logged_in? || (logged_in? && !current_user.admin?)
       flash[:danger] = "only admins can perform that action"
       redirect_to categories_path
-    else
-      render 'new'
     end
   end
 
