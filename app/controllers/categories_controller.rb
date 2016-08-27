db139d8f288c5551e6993d28164428b64b843467
Bryan Kahler <bkahler3@gmail.com>
2016-08-27 19:37:51 +0000

    added integration tests for categoriers

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,12 +1,32 @@
 class CategoriesController < ApplicationController
-  
+
   def index
+    @categories = Category.all
   end
-  
+
   def new
+    @category = Category.new
   end
-  
+
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      logger.debug "#{ "-" * 10} session: #{session.id}, class: #{self}, method: #{__method__}, message: created new category #{ "-" * 10}"
+      flash[:success] = "Category was succesfully added"
+      redirect_to categories_path
+    else
+      logger.warn "#{ "-" * 10} session: #{session.id}, class: #{self}, method: #{__method__}, message: category failed to create... #{ @category.errors.full_messages } #{ "-" * 10}"
+      render 'new'
+    end
+  end
+
   def show
-  end 
-  
-end 
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name)
+  end
+
+end
