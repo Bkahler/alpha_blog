456147519ba5bc479093a2d43c401ec3bfee6453
Bryan Kahler <bkahler3@gmail.com>
2016-09-04 04:27:08 +0000

    adding edit for categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,6 @@
 class CategoriesController < ApplicationController
   before_action :require_admin, except: [:index, :show]
+  before_action :set_category, except: [:index, :new, :create]
 
   def index
     @categories = Category.paginate(page: params["page"], per_page: 5)
@@ -26,6 +27,20 @@ class CategoriesController < ApplicationController
     @category_articles = @category.articles.paginate(page: params[:page], per_page: 5)
   end
 
+  def edit
+    @category = Category.find(params[:id])
+  end
+
+  def update
+    if @category.update(category_params)
+      logger.debug "#{ "-" * 10} session: #{session.id}, class: #{self}, method: #{__method__}, message: updated category #{ "-" * 10}"
+      flash[:success] = "Category: #{@category.name} successfully updated"
+      redirect_to category_path(@category)
+    else
+      render 'edit'
+    end
+  end
+
   private
 
   def category_params
@@ -37,6 +52,14 @@ class CategoriesController < ApplicationController
       flash[:danger] = "only admins can perform that action"
       redirect_to categories_path
     end
+  end
+
+  def set_category
+    @category = Category.find(params[:id])
+    rescue => e
+      logger.warn "#{ "-" * 10} session: #{session.id}, class: #{self}, method: #{__method__}, message: failed to find category #{params[:id]}...#{ e.message } #{ "-" * 10}"
+      flash[:danger] = "Failed to find category"
+      request_redirect(categories_path)
   end
 
 end
